1eec8d83ef65560dc44abf2e68cf624a1acdec58
shiquanJ <jiangshiquan2020@163.com>
2022-12-08 10:48:13 +0800

    Create By SG 12/8

---
+++ buyer/public/config.js
@@ -0,0 +1,17 @@
+var BASE = {
+    /**
+     * @description api请求基础路径
+     */
+    API_DEV: {
+      common: "http://172.16.9.180:8890",
+      buyer: "http://172.16.9.180:8888",
+      seller: "http://172.16.9.180:8889",
+      manager: "http://172.16.9.180:8887"
+    },
+    API_PROD: {
+      common: "https://common-api.pickmall.cn",
+      buyer: "https://buyer-api.pickmall.cn",
+      seller: "https://store-api.pickmall.cn",
+      manager: "https://admin-api.pickmall.cn"
+    },
+  };
