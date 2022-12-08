9934ec5d72154c82d41ab229cb7a9d09e74e6551
真锋 <rlawlsqhd1123@gmail.com>
2022-12-08 16:43:33 +0800

    dev 本地ip地址更换

--- seller/public/config.js
+++ seller/public/config.js
@@ -3,10 +3,10 @@ var BASE = {
    * @description api请求基础路径
    */
   API_DEV: {
-    common: "http://172.16.9.180:8890",
-    buyer: "http://172.16.9.180:8888",
-    seller: "http://172.16.9.180:8889",
-    manager: "http://172.16.9.180:8887"
+    common: "http://127.0.0.1:8890",
+    buyer: "http://127.0.0.1:8888",
+    seller: "http://127.0.0.1:8889",
+    manager: "http://127.0.0.1:8887"
   },
   API_PROD: {
     common: "https://common-api.pickmall.cn",
